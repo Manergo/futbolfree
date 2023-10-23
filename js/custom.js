f7118fc6f34df45a4ded65274366322926f2f782
Manergo <90414621+Manergo@users.noreply.github.com>
2023-10-23 16:34:15 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+// Get Current Year
+function getCurrentYear() {
+    var d = new Date();
+    var year = d.getFullYear();
+    document.querySelector("#displayDateYear").innerText = year;
+}
+getCurrentYear()
+
+//client section owl carousel
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: [
+        '<i class="fa fa-long-arrow-left" aria-hidden="true"></i>',
+        '<i class="fa fa-long-arrow-right" aria-hidden="true"></i>'
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+/** google_map js **/
+
+function myMap() {
+    const myLatLng = { lat: 38.989304950621005, lng: -1.8613691059836377};
+    const map = new google.maps.Map(document.getElementById("googleMap"), {
+        zoom: 16,
+        center: myLatLng,
+    });
+    
+    new google.maps.Marker({
+        position: myLatLng,
+        map,
+    })
+
+}
